b69043a6f09d3669fd119d7b98c98dd6110f517e
Daniel Kraus <krada@gmx.net>
2015-04-28 10:32:46 +0200

    Bump version number.
    - NEW: .deb package for PPA.

--- src/IndicatorIp/version.py
+++ src/IndicatorIp/version.py
@@ -1,4 +1,4 @@
 """Semantic version."""
 # To facilitate automatic extraction of the version using grep,
 # the version string should be enclosed in double quotes.
-VERSION = "1.1.0"
+VERSION = "2.0.0"
