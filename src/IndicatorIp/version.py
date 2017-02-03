bed49997bc81424878aa2bc9808885dd8e1a4a8c
Daniel Kraus <bovender@bovender.de>
2017-02-03 23:00:17 +0100

    Prepare 2.2.0 release.

--- src/IndicatorIp/version.py
+++ src/IndicatorIp/version.py
@@ -1,4 +1,4 @@
 """Semantic version."""
 # To facilitate automatic extraction of the version using grep,
 # the version string should be enclosed in double quotes.
-VERSION = "2.1.0"
+VERSION = "2.2.0"
